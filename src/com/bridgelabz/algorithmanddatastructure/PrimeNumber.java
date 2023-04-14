5872f9515c30a7e5736a002c7d6e61254f822c85
kiran68 <kiranurshil88@gmail.com>
2023-04-14 22:19:56 +0530

    Add: Check Onli Prime Numbers

---
+++ src/com/bridgelabz/algorithmanddatastructure/PrimeNumber.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.algorithmanddatastructure;
+
+public class PrimeNumber {
+	public static void main(String args[]) {
+		int range = 1000;
+		int num = 2;
+		while (num <= range) {
+			int i = 2;
+			while (i <= num / 2) {
+				if (num % i == 0) {
+					break;
+				}
+				i++;
+			}
+			if (i == (num / 2 + 1)) {
+				System.out.println(num + "");
+			}
+			num++;
+		}
+	}
+}
